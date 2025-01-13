b33a1b0be3ce8fbcb662910de8ff4e257454f3c6
Swenn Tetrel <stetrel@3D1.42angouleme.fr>
2025-01-13 20:56:09 +0100

    youpi jai les milli secondes

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,34 +6,35 @@
 /*   By: stetrel <stetrel@42angouleme.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 11:47:11 by stetrel           #+#    #+#             */
-/*   Updated: 2025/01/13 19:35:57 by stetrel          ###   ########.fr       */
+/*   Updated: 2025/01/13 20:55:37 by stetrel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
 #include <pthread.h>
-#include <stdatomic.h>
 #include <sys/time.h>
 
-
-long	get_time_in_ms(struct timeval time)
+long	get_ms(struct timeval val)
 {
-	return (time.tv_sec * 1000L + time.tv_usec / 1000L);
+	return (val.tv_sec * 1000) + (val.tv_usec / 1000);
 }
 
 void	*routine(void *arg)
 {
 	t_simulation *sim = (t_simulation*)arg;
 	struct timeval	tv;
+	long	res;
 
-	pthread_mutex_lock(&sim->mutex);
-	gettimeofday(&tv, NULL);
-	long	time_sim_ms = get_time_in_ms(sim->time);
-	long	time_routine_ms = get_time_in_ms(tv);
-	printf("time sim = %ld ms\n", time_sim_ms);
-	printf("time routine = %ld ms\n", time_routine_ms);
-	printf("elapsed time = %ld ms\n", time_routine_ms - time_sim_ms);
-	pthread_mutex_unlock(&sim->mutex);
+	while (1)
+	{
+		gettimeofday(&tv, NULL);
+		res = get_ms(tv);
+		long res2 = get_ms(sim->time);
+		pthread_mutex_lock(&sim->mutex);
+		printf("elapsed time = %ld ms\n", res - res2);
+		pthread_mutex_unlock(&sim->mutex);
+		// if philo dead: break
+	}
 	return (NULL);
 }
 
@@ -61,45 +62,25 @@ void	sim_init(t_simulation *simulation, t_philo **philos)
 	*philos = malloc(sizeof(t_philo) * simulation->data.nb_philo);
 	if (!*philos)
 		return ;
-	pthread_mutex_init(&simulation->mutex, NULL);
 	while (++i < simulation->data.nb_philo)
 	{
 		(*philos)[i].id = i;
-		pthread_create(&(*philos)[i].fork, NULL, routine, &simulation);
+		pthread_create(&(*philos)[i].fork, NULL, routine, simulation);
 	}
 }
 
-/*int	main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	t_philo				*philos;
 	int					i;
-	 t_simulation	simulation = {0};
+	t_simulation		simulation = {0};
 
 	if (check_data(argv, &simulation.data, argc))
 		return (1);
-	gettimeofday(&simulation.time, NULL);
 	sim_init(&simulation, &philos);
 	i = -1;
-	printf("jgl;kdfs = %ld\n", simulation.time.tv_usec);
+	gettimeofday(&simulation.time, NULL);
 	while (++i < simulation.data.nb_philo)
 		pthread_join(philos[i].fork, NULL);
-	pthread_mutex_destroy(&simulation.mutex);
 	free(philos);
-}*/
-
-int main(int argc, char **argv)
-{
-	t_simulation	simulation = {0};
-	struct timeval tv;
-	if (check_data(argv, &simulation.data, argc))
-		return (1);
-	gettimeofday(&simulation.time, NULL);
-	printf("sec = %lu | us = %lu\n", simulation.time.tv_usec, simulation.time.tv_sec);
-	gettimeofday(&tv, NULL);
-	printf("sec = %lu | us = %lu\n", tv.tv_usec, tv.tv_sec);
-
-	long	res_sec = simulation.time.tv_sec + tv.tv_sec;
-	long	res_usec = simulation.time.tv_usec + tv.tv_usec;
-	printf("sub_sec = %lu | sub_usec = %lu\n", res_sec, res_usec);
-
 }
